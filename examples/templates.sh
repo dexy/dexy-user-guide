b42b8eea323288a0240de6c7414314ef013ebf86
Ana Nelson <ana@ananelson.com>
2013-11-20 00:43:31 -0500

    Add output names example.

---
+++ examples/templates.sh
@@ -0,0 +1,16 @@
+### "custom-names"
+dexy gen -t custom-names -d custom-names
+
+### "cd"
+cd custom-names
+
+### "input-files"
+find .
+
+### "run-dexy"
+dexy setup
+dexy
+
+### "output-files"
+cd output/
+find .
