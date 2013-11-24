40cb15c48cc97012e4b979881d17bad6d7111839
Ana Nelson <ana@ananelson.com>
2013-11-23 20:44:16 -0800

    Add example of using template assertions.

---
+++ examples/assertions.sh
@@ -0,0 +1,25 @@
+### "generate"
+dexy gen -t assertions -d assertions
+cd assertions
+
+### "run-without-assertions"
+cp docs-without-assertions.md docs.md
+dexy
+
+### "save-results"
+mv output/docs.md docs-without-assertions-results.md
+
+### "setup-for-assertions"
+cp docs-with-assertions.md docs.md
+dexy reset
+
+### "run-with-assertions"
+dexy
+
+### "fix-broken-python"
+echo "print 'foo'" > broken.py
+dexy reset
+
+### "run-with-assertions-again"
+dexy
+cp output/docs.md docs-with-assertions-results.md
